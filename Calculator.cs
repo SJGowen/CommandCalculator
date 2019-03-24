28501e0090376c1ccc60d0d395937b34c9e98263
Stephen Gowen <sjgowen@gmail.com>
2019-03-24 15:59:34 +0000

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,244 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CommandCalculator
+{
+    public class Calculator
+    {
+        private readonly string _invalidExpression = "Invalid expression.";
+        private bool _floatingPointExpression;
+
+        public string Calculate(string equation)
+        {
+            equation = RemoveSpaces(equation);
+            _floatingPointExpression = equation.Contains('.');
+            if (!ParenthesisIsBalanced(equation)) return _invalidExpression;
+            equation = EvaluateParenthesisedPiecesOfEquation(equation);
+            return WeightedCalculate(equation);
+        }
+
+        private static string RemoveSpaces(string equation)
+        {
+            return equation.Replace(" ", string.Empty);
+        }
+
+        private static bool ParenthesisIsBalanced(string equation)
+        {
+            return equation.Count(x => x == '(') == equation.Count(x => x == ')');
+        }
+
+        private string EvaluateParenthesisedPiecesOfEquation(string equation)
+        {
+            while (equation.Contains("("))
+            {
+                var length = 0;
+                var startIndex = 0;
+                var equationIndex = 0;
+                foreach (var character in equation)
+                {
+                    if (character == '(')
+                    {
+                        startIndex = equationIndex + 1;
+                        length = 0;
+                    }
+                    else if (character == ')' && length == 0)
+                    {
+                        length = equationIndex - startIndex;
+                    }
+
+                    equationIndex += 1;
+                }
+
+                if (length <= 0) continue;
+                var subEquation = equation.Substring(startIndex, length);
+                var result = WeightedCalculate(subEquation);
+                equation = equation.Replace("(" + subEquation + ")", result);
+            }
+
+            return equation;
+        }
+
+        private string WeightedCalculate(string equation)
+        {
+            if (equation.Length == 0) return string.Empty;
+            if (equation.Contains(_invalidExpression)) return _invalidExpression;
+
+            var bits = BreakUpEquation(equation);
+            ApplyNegatives(bits);
+            if (bits.Count % 2 == 0) return _invalidExpression; // Valid expression have an odd number of bits
+            if (bits.Count <= 1) return bits.Count == 1 ? bits[0] : _invalidExpression;
+            CondenseListByDoing(bits, "^");
+            CondenseListByDoing(bits, "/%*");
+            CondenseListByDoing(bits, "+-");
+            return bits.Count == 1 ? bits[0] : _invalidExpression;
+        }
+
+        private static List<string> BreakUpEquation(string equation)
+        {
+            var bit = string.Empty;
+            var bits = new List<string>();
+            foreach (var character in equation)
+            {
+                if (int.TryParse(character.ToString(), out var _) || character == '.')
+                {
+                    bit += character;
+                }
+                else
+                {
+                    if (bit != string.Empty)
+                    {
+                        bits.Add(bit);
+                        bit = string.Empty;
+                    }
+
+                    bits.Add(character.ToString());
+                }
+            }
+
+            if (bit != string.Empty) bits.Add(bit);
+
+            return bits;
+        }
+
+        private void ApplyNegatives(IList<string> bits)
+        {
+            if (bits.IndexOf("-") == -1) return;
+            var bitIndex = 0;
+            while (bitIndex < bits.Count)
+            {
+                if (bits[bitIndex] == "-" && NoNumberPrecedesThisCharacter(bits, bitIndex))
+                {
+                    if (_floatingPointExpression)
+                    {
+                        if (float.TryParse(bits[bitIndex + 1], out var float1))
+                        {
+                            bits[bitIndex + 1] = (float1 * -1).ToString("F");
+                            bits.RemoveAt(bitIndex);
+                        }
+                    }
+                    else
+                    {
+                        if (int.TryParse(bits[bitIndex + 1], out var integer1))
+                        {
+                            bits[bitIndex + 1] = (integer1 * -1).ToString();
+                            bits.RemoveAt(bitIndex);
+                        }
+                    }
+                }
+
+                bitIndex++;
+            }
+        }
+
+        private static bool NoNumberPrecedesThisCharacter(IList<string> bits, int bitIndex)
+        {
+            return bitIndex == 0 || !float.TryParse(bits[bitIndex - 1], out var _);
+        }
+
+        private void CondenseListByDoing(List<string> bits, string mathsOperator)
+        {
+            if (bits.Count == 1) return;
+
+            var bitIndex = 1; // Looking at operators only, hence start at 1 and increment by 2
+            while (bitIndex < bits.Count)
+            {
+                foreach (var mathsOp in mathsOperator)
+                {
+                    WriteDebugMessageAndArray($"Looking at = '{bits[bitIndex]}'.\tLooking for = '{mathsOp.ToString()}'.\t", bits);
+                    if (bits[bitIndex] == mathsOp.ToString())
+                    {
+                        if (_floatingPointExpression)
+                        {
+                            CondenseAsFloat(bits, bitIndex);
+                        }
+                        else
+                        {
+                            CondenseAsInt(bits, bitIndex);
+                        }
+
+                        bitIndex -= 2;
+                        break;
+                    }
+                }
+
+                bitIndex += 2;
+            }
+
+            WriteDebugMessageAndArray($"After ConsolidateListByDoing ({mathsOperator}).\t", bits);
+        }
+
+        private static void WriteDebugMessageAndArray(string message, List<string> bits)
+        {
+            if (!Debugger.IsAttached) return;
+            Debug.WriteLine($"{message}Bits = '{string.Join(' ', bits)}'.");
+        }
+
+        private void CondenseAsFloat(IList<string> bits, int bitIndex)
+        {
+            if (!float.TryParse(bits[bitIndex - 1], out var float1) ||
+                (!float.TryParse(bits[bitIndex + 1], out var float2))) return;
+            switch (bits[bitIndex])
+            {
+                case "^":
+                    bits[bitIndex] = Math.Pow(float1, float2).ToString("F");
+                    break;
+                case "*":
+                    bits[bitIndex] = (float1 * float2).ToString("F");
+                    break;
+                case "/":
+                    bits[bitIndex] = (float1 / float2).ToString("F");
+                    break;
+                case "%":
+                    bits[bitIndex] = (float1 % float2).ToString("F");
+                    break;
+                case "+":
+                    bits[bitIndex] = (float1 + float2).ToString("F");
+                    break;
+                case "-":
+                    bits[bitIndex] = (float1 - float2).ToString("F");
+                    break;
+                default:
+                    bits[bitIndex] = _invalidExpression;
+                    break;
+            }
+
+            bits.RemoveAt(bitIndex + 1);
+            bits.RemoveAt(bitIndex - 1);
+        }
+
+        private void CondenseAsInt(IList<string> bits, int bitIndex)
+        {
+            if (!int.TryParse(bits[bitIndex - 1], out var integer1) ||
+                (!int.TryParse(bits[bitIndex + 1], out var integer2))) return;
+            switch (bits[bitIndex])
+            {
+                case "^":
+                    bits[bitIndex] = Math.Pow(integer1, integer2).ToString("F0");
+                    break;
+                case "*":
+                    bits[bitIndex] = (integer1 * integer2).ToString();
+                    break;
+                case "/":
+                    bits[bitIndex] = (integer1 / integer2).ToString();
+                    break;
+                case "%":
+                    bits[bitIndex] = (integer1 % integer2).ToString();
+                    break;
+                case "+":
+                    bits[bitIndex] = (integer1 + integer2).ToString();
+                    break;
+                case "-":
+                    bits[bitIndex] = (integer1 - integer2).ToString();
+                    break;
+                default:
+                    bits[bitIndex] = _invalidExpression;
+                    break;
+            }
+
+            bits.RemoveAt(bitIndex + 1);
+            bits.RemoveAt(bitIndex - 1);
+        }
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,78 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommandCalculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        [DataRow("", "")]
+        [DataRow("", " ")]
+        [DataRow("1", "1")]
+        [DataRow("123", "123")]
+        [DataRow("2", "1+1")]
+        [DataRow("444", "123+321")]
+        [DataRow("15", "1+2 + 3 +4+5")]
+        [DataRow("5", "1+2 + 3 +4-5")]
+        [DataRow("26", "1+2 + 3 +4*5")]
+        [DataRow("8", "1+2 + 3 +10/5")]
+        [DataRow("10", "1+2+2 + 3 +10/5")]
+        [DataRow("13", "1+2+3 + 3 +4%5")]
+        [DataRow("7", "1+2+5 % 3 +12%5")]
+        [DataRow("21", "1+4^2+5 % 3 +12%5")]
+        public void CalculationsTests(string expectedResult, string equation)
+        {
+            var calc = new Calculator();
+            var result = calc.Calculate(equation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow("3", "1+5-(6/2)")]
+        [DataRow("6", "10+5-((6/2)*(6/2))")]
+        [DataRow("6", "10+5-(6/2)*(6/2)")]
+        public void CalculationsWithBracketsTests(string expectedResult, string equation)
+        {
+            var calc = new Calculator();
+            var result = calc.Calculate(equation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow("-46", "-23+-23")]
+        [DataRow("0", "-23+23")]
+        [DataRow("4", "-24/-6")]
+        [DataRow("-4", "24/-6")]
+        public void CalculationsWithNegativeNumbersTests(string expectedResult, string equation)
+        {
+            var calc = new Calculator();
+            var result = calc.Calculate(equation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow("Invalid expression.", "-23+&23")]
+        [DataRow("Invalid expression.", "-23$23")]
+        [DataRow("Invalid expression.", "-24//6")]
+        [DataRow("Invalid expression.", "1+(24/-6))")]
+        public void CalculationsWithErrorsTests(string expectedResult, string equation)
+        {
+            var calc = new Calculator();
+            var result = calc.Calculate(equation);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [DataRow("3.00", "24.3/8.1")]
+        [DataRow("3.00", "24.0/8.0")]
+        [DataRow("31.50", "4.5*7")]
+        [DataRow("31.50", "8%5-3+4.5*7")]
+        [DataRow("27.00", "3.0^3")]
+        public void CalculationsForFloatingPointTests(string expectedResult, string equation)
+        {
+            var calc = new Calculator();
+            var result = calc.Calculate(equation);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
